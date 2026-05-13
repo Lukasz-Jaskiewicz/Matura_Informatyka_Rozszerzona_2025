2fd84dbefbbd19648ae5027bd6161e869f439473
Lukasz Jaskiewicz <lukasz.jaskiewicz2@gmail.com>
2026-05-13 20:22:15 +0200

    Dodaje glowny plik aplikacji

---
+++ src/app.py
@@ -0,0 +1,11 @@
+import Zadanie_1
+import Zadanie_2
+import Zadanie_3
+import Zadanie_6
+import Zadanie_7
+
+Zadanie_1.Zapisz_Rozwiazanie_3_Podzadan()
+Zadanie_2.Zapisz_Rozwiazanie_4_Podzadan()
+Zadanie_3.Zapisz_rozwiazanie_2_podzadan()
+Zadanie_6.Zapisz_rozwiazanie_5_podzadan()
+Zadanie_7.Zapisz_rozwiazanie_4_podzadan()
